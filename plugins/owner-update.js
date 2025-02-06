a9328f789b00353de1ee1652bb3929e7b5971533
Stiick777 <kanixxxcexx@gmail.com>
2025-02-06 13:48:49 -0500

    Add files via upload

--- plugins/owner-update.js
+++ plugins/owner-update.js
@@ -1,30 +1,43 @@
 import { execSync } from 'child_process'
+import fs from 'fs'
 
 var handler = async (m, { conn, text }) => {
-  m.react('🚀') 
+  m.react('🚀')
   try {
-    // Guardar los cambios locales temporalmente
-    execSync('git stash')
+    // Verifica si la carpeta .git/tmp existe antes de actualizar
+    const tmpExists = fs.existsSync('.git/tmp')
+
+    // Verifica si hay cambios locales antes de hacer git stash
+    const status = execSync('git status --porcelain').toString().trim()
+    if (status) {
+      execSync('git stash')
+    }
 
     // Actualizar el repositorio
-    const stdout = execSync('git pull --rebase' + (m.fromMe && text ? ' ' + text : ''));
+    const stdout = execSync('git pull --rebase' + (m.fromMe && text ? ' ' + text : ''))
     let messager = stdout.toString()
 
-    // Restaurar los cambios locales
-    execSync('git stash pop')
+    // Restaurar los cambios locales si se hizo stash
+    if (status) {
+      execSync('git stash pop')
+    }
 
-    if (messager.includes('☘️ Ya estoy actualizada.')) messager = '☘️ Ya estoy actualizada a la última versión.'
-    if (messager.includes('💫 Actualizando.')) messager = '✨️ Procesando, espere un momento mientras me actualizo.\n\n' + stdout.toString()
+    // Si la carpeta .git/tmp se eliminó, la recreamos
+    if (tmpExists && !fs.existsSync('.git/tmp')) {
+      fs.mkdirSync('.git/tmp', { recursive: true })
+    }
+
+    if (messager.includes('Already up to date')) messager = '☘️ Ya estoy actualizada a la última versión.'
+    if (messager.includes('Actualizando.')) messager = '✨️ Procesando, espere un momento mientras me actualizo.\n\n' + stdout.toString()
 
     conn.reply(m.chat, messager, m, rcanal,)
 
-  } catch (error) { 
+  } catch (error) {
     try {
-      const status = execSync('git status --porcelain')
+      const status = execSync('git status --porcelain').toString().trim()
 
       if (status.length > 0) {
-        const conflictedFiles = status.toString().split('\n')
-          .filter(line => line.trim() !== '')
+        const conflictedFiles = status.split('\n')
           .map(line => '*→ ' + line.slice(3) + '*')
           .filter(Boolean)
 
@@ -37,7 +50,7 @@ var handler = async (m, { conn, text }) => {
       console.error(error)
       let errorMessage2 = '⚠️ Ocurrió un error inesperado.'
       if (error.message) {
-        errorMessage2 += '\n⚠️ Mensaje de error: ' + error.message;
+        errorMessage2 += '\n⚠️ Mensaje de error: ' + error.message
       }
       await conn.reply(m.chat, errorMessage2, m, rcanal,)
     }
