d6d111a9ff479949d4c64374600a5d3fec0a19d1
Stiick777 <kanixxxcexx@gmail.com>
2025-02-16 20:01:54 -0500

    Add files via upload

--- plugins/descargas-ytmp4doc.js
+++ plugins/descargas-ytmp4doc.js
@@ -2,10 +2,10 @@ import fetch from 'node-fetch';
 
 let handler = async (m, { conn: star, args }) => {
   if (!args || !args[0]) 
-    return star.reply(m.chat, '💣 _*Ingresa el enlace del video de YouTube junto al comando.*_\n\n`Ejemplo:`\n> *!ytmp4doc* https://youtube.com/watch?v=qHDJSRlNhVs', m, );
+    return star.reply(m.chat, '💣 _*Ingresa el enlace del video de YouTube junto al comando.*_\n\n`Ejemplo:`\n> *!ytmp4doc* https://youtube.com/watch?v=qHDJSRlNhVs', m, rcanal);
 
   if (!args[0].match(/youtu/gi)) 
-    return star.reply(m.chat, `Verifica que el enlace sea de YouTube.`, m, ).then(() => m.react('✖️'));
+    return star.reply(m.chat, `Verifica que el enlace sea de YouTube.`, m, rcanal).then(() => m.react('✖️'));
 
   await m.react('🕓');
   try {
@@ -23,7 +23,7 @@ let handler = async (m, { conn: star, args }) => {
     txt += `	🎥   *Calidad*: ${quality}\n\n`;
     txt += `> ️ *Se está enviando su video, por favor espere*`;
 
-    await star.reply(m.chat, txt, m, );
+    await star.reply(m.chat, txt, m,);
     await star.sendMessage(m.chat, {
       document: { url: download_url }, // Enlace directo para descargar el archivo como documento
       caption: `${title}\n🎥 *Calidad*: ${quality}\n\n*🌝 *Provided by KanBot* 🌚`,
@@ -32,6 +32,27 @@ let handler = async (m, { conn: star, args }) => {
     }, { quoted: m });
     await m.react('✅');
 } catch (e) {
+    
+    try {
+    let v = args[0];
+
+    let apiResponse = await fetch(`https://api.agungny.my.id/api/youtube-video?url=${encodeURIComponent(v)}`);
+    let data = await apiResponse.json();
+
+    if (data.status !== "true") throw new Error('Error en la descarga');
+
+    let { title, downloadUrl } = data.result;
+
+    await star.sendMessage(m.chat, {
+      document: { url: downloadUrl }, // Enlace directo para descargar el archivo como documento
+      caption: `${title}\n\n*🌝 *Provided by KanBot* 🌚`,
+      mimetype: 'video/mp4',
+      fileName: `${title}.mp4` // Nombre del archivo con extensión
+    }, { quoted: m });
+    await m.react('✅');
+} catch (e) {
+    
+}
     console.error(e);
     await m.react('✖️');
     star.reply(m.chat, '❌ _*Error al procesar el enlace. Por favor, intenta de nuevo.*_', m);
