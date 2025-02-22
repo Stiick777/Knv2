6c232f176dff41ef132c0b550bc6fb8fdf56ce8d
Stiick777 <kanixxxcexx@gmail.com>
2025-02-22 02:14:00 -0500

    Add files via upload

--- plugins/ia-dalle.js
+++ plugins/ia-dalle.js
@@ -1,23 +1,34 @@
 import fetch from 'node-fetch';
 
-const handler = async (m, {conn, text, usedPrefix, command}) => {
-    if (!text) throw `*🧧 ingrese una petición para generar una imagen con dalle...*`;
-    m.react('🕒')
-    await conn.sendMessage(m.chat, {text: '*🧧 Espere un momento...*'}, {quoted: m});
-    
-    try {
-        const response = await fetch(`https://api-xovvip.vercel.app/text2img?text=${encodeURIComponent(text)}`);
-        if (!response.ok) throw new Error('Network response was not ok');
-        
-        const buffer = await response.buffer();
-        m.react('☑️')
-        await conn.sendMessage(m.chat, {image: buffer}, {quoted: m});
-    } catch {
-        throw `Error...`;
+const handler = async (m, { conn, text }) => {
+    if (!text) {
+        await conn.sendMessage(m.chat, { text: '*🧧 Ingrese una petición para generar una imagen con AI...*' }, { quoted: m });
+        return;
     }
-}
-handler.tags = ['ai']
-handler.help = ['dalle']
+
+    m.react('🕒');
+    await conn.sendMessage(m.chat, { text: '*🧧 Generando imagen, por favor espere...*' }, { quoted: m });
+
+    try {
+        const response = await fetch(`https://bk9.fun/ai/magicstudio?prompt=${encodeURIComponent(text)}`);
+        
+        if (!response.ok) {
+            await conn.sendMessage(m.chat, { text: '*❌ Error al generar la imagen. Inténtelo de nuevo más tarde.*' }, { quoted: m });
+            return;
+        }
+
+        const buffer = await response.buffer();
+        m.react('✅');
+        await conn.sendMessage(m.chat, { image: buffer }, { quoted: m });
+
+    } catch (error) {
+        await conn.sendMessage(m.chat, { text: '*❌ Ocurrió un error al procesar la solicitud.*' }, { quoted: m });
+    }
+};
+
+handler.tags = ['ai'];
+handler.help = ['dalle'];
 handler.command = ['dalle'];
-handler. group = true;
+handler.group = true;
+
 export default handler;
