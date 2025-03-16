2135ead834753ec9f6f5c534776a10cf7c3b049d
Stiick777 <kanixxxcexx@gmail.com>
2025-03-15 22:01:50 -0500

    Add files via upload

--- plugins/sticker-brat.js
+++ plugins/sticker-brat.js
@@ -1,44 +1,39 @@
 import { sticker } from '../lib/sticker.js';
 import axios from 'axios';
 
-const handler = async (m, { conn, args, usedPrefix, command }) => {
-    let text;
-    
-    if (args.length >= 1) {
-        text = args.join(" ");
-    } else if (m.quoted && m.quoted.text) {
-        text = m.quoted.text;
+const handler = async (m, { conn, args }) => {
+  let text;
+  
+  if (args.length >= 1) {
+    text = args.join(' ');
+  } else if (m.quoted && m.quoted.text) {
+    text = m.quoted.text;
+  } else {
+    return conn.reply(m.chat, '💡 Te faltó el texto!', m);
+  }
+
+  if (text.length > 40) return conn.reply(m.chat, '⚠️ El texto no puede tener más de 40 caracteres.', m);
+
+  try {
+    const apiUrl = `https://api.agungny.my.id/api/bratv1?q=${encodeURIComponent(text)}`;
+    const response = await axios.get(apiUrl, { responseType: 'arraybuffer' });
+
+    if (!response.data) throw new Error('No se pudo obtener la imagen.');
+
+    const stickerBuffer = await sticker(response.data, false, global.packsticker, global.author);
+
+    if (stickerBuffer) {
+      await conn.sendFile(m.chat, stickerBuffer, 'sticker.webp', '', m, { asSticker: true });
     } else {
-        return conn.reply(m.chat, '💡 Te Faltó El Texto!', m);
+      throw new Error('Error al convertir la imagen en sticker.');
     }
-    
-    if (text.length > 40) {
-        return conn.reply(m.chat, '⚠️ El texto no puede tener más de 40 caracteres', m);
-    }
-
-    try {
-        const response = await axios.get(`https://api.agungny.my.id/api/bratv1?q=${encodeURIComponent(text)}`);
-        
-        if (!response.data || !response.data.result) {
-            return conn.reply(m.chat, '⚠️ No se pudo generar la imagen.', m);
-        }
-
-        const buffer = await axios.get(response.data.result, { responseType: 'arraybuffer' });
-        let stiker = await sticker(buffer.data, false, global.packsticker, global.author);
-
-        if (stiker) {
-            return conn.sendFile(m.chat, stiker, 'sticker.webp', '', m);
-        } else {
-            return conn.reply(m.chat, '⚠️ No se pudo convertir en sticker.', m);
-        }
-    } catch (error) {
-        console.error(error);
-        return conn.reply(m.chat, '❌ Ocurrió un error al procesar tu solicitud.', m);
-    }
+  } catch (error) {
+    console.error(error);
+    conn.reply(m.chat, '❌ Ocurrió un error al generar el sticker.', m);
+  }
 };
 
-handler.help = ['qc'];
+handler.help = ['brat'];
 handler.tags = ['sticker'];
 handler.command = ['brat'];
-
 export default handler;
