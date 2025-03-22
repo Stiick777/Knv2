edbe47971807f1a5edc97702ec56dc04c403d36c
Stiick777 <kanixxxcexx@gmail.com>
2025-03-21 19:09:14 -0500

    Update rpg-depositar.js

--- plugins/rpg-depositar.js
+++ plugins/rpg-depositar.js
@@ -2,25 +2,25 @@ import db from '../lib/database.js'
 
 let handler = async (m, { args }) => {
 let user = global.db.data.users[m.sender]
-if (!args[0]) return m.reply('🚩 Ingresa la cantidad de *⭐ Estrellas* que deseas Depositar.')
-if ((args[0]) < 1) return m.reply('🚩 Ingresa una cantidad válida de *⭐ Estrellas.')
+if (!args[0]) return m.reply('🚩 Ingresa la cantidad de *⭐ Estrellas* que deseas Depositar.', m, rcanal)
+if ((args[0]) < 1) return m.reply('🚩 Ingresa una cantidad válida de *⭐ Estrellas.', m, rcanal)
 if (args[0] == 'all') {
 let count = parseInt(user.estrellas)
 user.estrellas -= count * 1
 user.bank += count * 1
-await m.reply(`Depositaste *${count} ⭐ Estrellas* al Banco.`)
+await m.reply(`Depositaste *${count} ⭐ Estrellas* al Banco.`, m, rcanal)
 return !0
 }
-if (!Number(args[0])) return m.reply('🚩 La cantidad deve ser un Numero.')
+if (!Number(args[0])) return m.reply('🚩 La cantidad deve ser un Numero.', m, rcanal)
 let count = parseInt(args[0])
-if (!user.estrellas) return m.reply('No tienes *⭐ Estrellas* en la Cartera.')
-if (user.estrellas < count) return m.reply(`Solo tienes *${user.estrellas} ⭐ Estrellas* en la Cartera.`)
+if (!user.estrellas) return m.reply('No tienes *⭐ Estrellas* en la Cartera.', m, rcanal)
+if (user.estrellas < count) return m.reply(`Solo tienes *${user.estrellas} ⭐ Estrellas* en la Cartera.`, m, rcanal)
 user.estrellas -= count * 1
 user.bank += count * 1
-await m.reply(`Depositaste *${count} ⭐ Estrellas* al Banco.`)}
+await m.reply(`Depositaste *${count} ⭐ Estrellas* al Banco.`, m, rcanal)}
 
 handler.help = ['depositar']
 handler.tags = ['rpg']
-handler.command = ['deposit', 'depositar', 'dep', 'aguardar']
+handler.command = ['deposit', 'depositar', 'dep', 'd']
 handler.group = true;
 export default handler 
