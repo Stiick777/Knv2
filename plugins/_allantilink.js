1a92171756c54c2e152e280472bedb0545fb6752
Stiick777 <kanixxxcexx@gmail.com>
2025-03-23 19:42:58 -0500

    Update _allantilink.js

--- plugins/_allantilink.js
+++ plugins/_allantilink.js
@@ -1,4 +1,4 @@
-import fetch from 'node-fetch'  
+import fetch from 'node-fetch'
 const isLinkTik = /tiktok.com/i 
 const isLinkYt = /youtube.com|youtu.be/i 
 const isLinkTel = /telegram.com/i 
@@ -8,8 +8,7 @@ const isLinkTw = /twitter.com/i
   
 let handler = m => m
 handler.before = async function (m, { conn, args, usedPrefix, command, isAdmin, isBotAdmin }) {
-if (m.isBaileys && m.fromMe)
-return !0
+if (m.isBaileys && m.fromMe) return !0
 if (!m.isGroup) return !1
 let chat = global.db.data.chats[m.chat]
 let bot = global.db.data.settings[this.user.jid] || {}
@@ -24,72 +23,27 @@ const isAntiLinkTel = isLinkTel.exec(m.text)
 const isAntiLinkFb = isLinkFb.exec(m.text)
 const isAntiLinkIg = isLinkIg.exec(m.text)
 const isAntiLinkTw = isLinkTw.exec(m.text)
- 
-if (chat.antiTiktok && isAntiLinkTik) {  
-if (isBotAdmin && bot.restrict) {
-await conn.reply(m.chat, `『✦』Se detecto un enlace de \`TikTok\`.\nSeras eliminado/a: *@${toUser}*`, null, { mentions: [aa] })
-await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet }})
-await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
-} else if (!isBotAdmin) {
-return m.reply(`『✦』El bot no es admin, no puedo eliminar personas.`)
-} else if (!bot.restrict) {
-return m.reply(`『✦』Las restricciones no estan activas.`)
-}}
-    
-if (chat.antiYoutube && isAntiLinkYt) {
-if (isBotAdmin && bot.restrict) {
-await conn.reply(m.chat, `『✦』Se detecto un enlace de \`YouTube\`.\nSeras eliminado/a: *@${toUser}*`, null, { mentions: [aa] })
-await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet }})
-await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
-} else if (!isBotAdmin) {
-return m.reply(`『✦』El bot no es admin, no puedo eliminar personas.`)
-} else if (!bot.restrict) {
-return m.reply(`『✦』Las restricciones no estan activas.`)
-}}  
-    
-if (chat.antiTelegram && isAntiLinkTel) {
-if (isBotAdmin && bot.restrict) {
-await conn.reply(m.chat, `『✦』Se detecto un enlace de \`Telegram\`.\nSeras eliminado/a: *@${toUser}*`, null, { mentions: [aa] })
-await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet }})
-await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
-} else if (!isBotAdmin) {
-return m.reply(`『✦』El bot no es admin, no puedo eliminar personas.`)
-} else if (!bot.restrict) {
-return m.reply(`『✦』Las restricciones no estan activas.`)
-}}    
-    
-if (chat.antiFacebook && isAntiLinkFb) {
-if (isBotAdmin && bot.restrict) {
-await conn.reply(m.chat, `『✦』Se detecto un enlace de \`Facebook\`.\nSeras eliminado/a: *@${toUser}*`, null, { mentions: [aa] })
-await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet }})
-await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
-} else if (!isBotAdmin) {
-return m.reply(`『✦』El bot no es admin, no puedo eliminar personas.`)
-} else if (!bot.restrict) {
-return m.reply(`『✦』Las restricciones no estan activas.`)
-}}  
-    
-if (chat.antiInstagram && isAntiLinkIg) {
-if (isBotAdmin && bot.restrict) {
-await conn.reply(m.chat, `『✦』Se detecto un enlace de \`Instagram\`.\nSeras eliminado/a: *@${toUser}*`, null, { mentions: [aa] })
-await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet }})
-await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
-} else if (!isBotAdmin) {
-return m.reply(`『✦』El bot no es admin, no puedo eliminar personas.`)
-} else if (!bot.restrict) {
-return m.reply(`『✦』Las restricciones no estan activas.`)
-}}
-    
-if (chat.antiTwitter && isAntiLinkTw) {
-if (isBotAdmin && bot.restrict) {
-await conn.reply(m.chat, `『✦』Se detecto un enlace de \`Twitter\`.\nSeras eliminado/a: *@${toUser}*`, null, { mentions: [aa] })
-await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet }})
-await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
-} else if (!isBotAdmin) {
-return m.reply(`『✦』El bot no es admin, no puedo eliminar personas.`)
-} else if (!bot.restrict) {
-return m.reply(`『✦』Las restricciones no estan activas.`)
-}}
+
+async function eliminarUsuario(plataforma, regex) {
+    if (chat[`anti${plataforma}`] && regex) {  
+        if (isBotAdmin) {
+            await conn.reply(m.chat, `『✦』Se detectó un enlace de \`${plataforma}\`.\nSerás eliminado/a: *@${toUser}*`, null, { mentions: [aa] })
+            await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet }})
+            await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
+        } else {
+            return m.reply(`『✦』El bot no es admin, no puedo eliminar personas.`)
+        }
+    }
+}
+
+await eliminarUsuario("Tiktok", isAntiLinkTik)
+await eliminarUsuario("Youtube", isAntiLinkYt)
+await eliminarUsuario("Telegram", isAntiLinkTel)
+await eliminarUsuario("Facebook", isAntiLinkFb)
+await eliminarUsuario("Instagram", isAntiLinkIg)
+await eliminarUsuario("Twitter", isAntiLinkTw)
+
 return !0
 }
+
 export default handler
