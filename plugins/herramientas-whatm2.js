39f938c28bd4395943f3532d3bebca835f541d32
Stiick777 <kanixxxcexx@gmail.com>
2025-04-16 21:54:34 -0500

    Update herramientas-whatm2.js

--- plugins/herramientas-whatm2.js
+++ plugins/herramientas-whatm2.js
@@ -2,7 +2,7 @@ import fs from 'fs'
 import fetch from 'node-fetch'
 import FormData from 'form-data'
 
-const API_TOKEN = 'f22ae2c9d66163ef0396c162320db880' // Tu API Key de Audd.io
+const API_TOKEN = 'a599ae671c01e6c560280366c33838c7' // Tu API Key de Audd.io
 
 let handler = async (m, { conn }) => {
     try {
