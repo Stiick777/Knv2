c65e0fd535aa5fcb14ea1ed5b2712af71de9d6d9
Stiick777 <kanixxxcexx@gmail.com>
2025-06-14 00:26:39 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,79 +1,54 @@
-import { join, dirname } from 'path';
-import { createRequire } from 'module';
-import { fileURLToPath } from 'url';
-import { setupMaster, fork } from 'cluster';
-import cfonts from 'cfonts';
-import readline from 'readline';
-import yargs from 'yargs';
-import chalk from 'chalk'; 
-import fs from 'fs'; 
-import './config.js'; //max update 2025
+console.log('✯ Iniciando ✯')
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
-const require = createRequire(__dirname);
-const { say } = cfonts;
-const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
-let isRunning = false;
+import { join, dirname } from 'path'
+import { createRequire } from 'module'
+import { fileURLToPath } from 'url'
+import { setupMaster, fork } from 'cluster'
+import { watchFile, unwatchFile } from 'fs'
+import cfonts from 'cfonts'
 
-const question = (texto) => new Promise((resolver) => rl.question(texto, resolver));
+const __dirname = dirname(fileURLToPath(import.meta.url))
+const require = createRequire(__dirname)
 
-console.log(chalk.yellow.bold('—◉ㅤIniciando sistema...'));
+cfonts.say('Ai\nHoshino', {
+  font: 'chrome',
+  align: 'center',
+  gradient: ['red', 'magenta']
+})
 
-async function start(file) {
-  if (isRunning) return;
-  isRunning = true;
+cfonts.say(`WhatsApp Bot Multi Device`, {
+  font: 'console',
+  align: 'center',
+  gradient: ['red', 'magenta']
+})
 
-  say('The Mystic', {
-    font: 'chrome',
-    align: 'center',
-    gradient: ['red', 'magenta'],
-  });
+let isRunning = false
 
-  say(`Creado por Bruno Sobrino`, {
-    font: 'console',
-    align: 'center',
-    gradient: ['red', 'magenta'],
-  });
+async function start(files) {
+  if (isRunning) return
+  isRunning = true
 
-  const args = [join(__dirname, file), ...process.argv.slice(2)];
-  setupMaster({ exec: args[0], args: args.slice(1) });
+  for (const file of files) {
+    const args = [join(__dirname, file), ...process.argv.slice(2)]
 
-  const p = fork();
+    setupMaster({
+      exec: args[0],
+      args: args.slice(1),
+    })
 
-  p.on('message', (data) => {
-    switch (data) {
-      case 'reset':
-        p.process.kill();
-        isRunning = false;
-        start.apply(this, arguments);
-        break;
-      case 'uptime':
-        p.send(process.uptime());
-        break;
-    }
-  });
+    let p = fork()
 
-  p.on('exit', (_, code) => {
-    isRunning = false;
-    console.error(chalk.red.bold('[ ERROR ] Ocurrió un error inesperado:'), code);
-    p.process.kill();
-    isRunning = false;
-    start.apply(this, arguments);
-    if (process.env.pm_id) {
-      process.exit(1);
-    } else {
-      process.exit();
-    }
-  });
+    p.on('exit', (code) => {
+      isRunning = false
+      start(files)
 
-  const opts = new Object(yargs(process.argv.slice(2)).exitProcess(false).parse());
-  if (!opts['test']) {
-    if (!rl.listenerCount()) {
-      rl.on('line', (line) => {
-        p.emit('message', line.trim());
-      });
-    }
+      if (code === 0) return
+      watchFile(args[0], () => {
+        unwatchFile(args[0])
+        start(files)
+      })
+    })
   }
 }
 
-start('main.js');
+start(['starlights.js'])
