7da8f6186dd41b258cad1c13a2053d5782d2ae00
Stiick777 <kanixxxcexx@gmail.com>
2025-09-27 23:28:52 -0500

    Update gc-delete.js

--- plugins/gc-delete.js
+++ plugins/gc-delete.js
@@ -1,18 +1,20 @@
-let handler = async (m, { conn, usedPrefix, command }) => {
-
-if (!m.quoted) return conn.reply(m.chat, `🚩 Responde al mensaje que deseas eliminar.`, m, rcanal)
+let handler = async (m, { conn, command }) => {
+if (!m.quoted) {
+return conn.reply(m.chat, `❀ Por favor, cita el mensaje que deseas eliminar.`, m)
+}
 try {
-let delet = m.message.extendedTextMessage.contextInfo.participant
-let bang = m.message.extendedTextMessage.contextInfo.stanzaId
-return conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet }})
+let participant = m.message.extendedTextMessage.contextInfo.participant
+let stanzaId = m.message.extendedTextMessage.contextInfo.stanzaId
+return conn.sendMessage(m.chat, {
+delete: { remoteJid: m.chat, fromMe: false, id: stanzaId, participant: participant }})
 } catch {
-return conn.sendMessage(m.chat, { delete: m.quoted.vM.key })
+return conn.sendMessage(m.chat, { delete: m.quoted.key })
 }}
 
 handler.help = ['delete']
 handler.tags = ['grupo']
-handler.command = ['del','delete']
-handler.group = false
+handler.command = ['del', 'delete']
+handler.group = true
 handler.admin = true
 handler.botAdmin = true
 
