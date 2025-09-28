e2753c9f81727d8d90942a22a06cde46be65bad5
Stiick777 <kanixxxcexx@gmail.com>
2025-09-27 23:29:45 -0500

    Update gc-link.js

--- plugins/gc-link.js
+++ plugins/gc-link.js
@@ -1,14 +1,14 @@
 var handler = async (m, { conn, args }) => {
-
 let group = m.chat
+const pp = await conn.profilePictureUrl(group, 'image').catch((_) => 'https://files.catbox.moe/xr2m6u.jpg')
 let link = 'https://chat.whatsapp.com/' + await conn.groupInviteCode(group)
-conn.reply(m.chat, '🚩\v' + link, m, rcanal, { detectLink: true })
-
+let message = `\t*⌁☍꒷₊˚ Link del Grupo ꒷₊˚⌁☍*\n\n> \`Link:\` ${link}`
+await conn.sendMessage(group, { image: { url: pp }, caption: message })
 }
+
 handler.help = ['link']
 handler.tags = ['grupo']
-handler.command = ['link','linkgroup']
-handler.admin = true;
+handler.command = ['link', 'enlace']
 handler.group = true
 handler.botAdmin = true
 
