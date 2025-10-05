66482167682df24a0b85b9b5bc8acec0c8a3b399
Stiick777 <kanixxxcexx@gmail.com>
2025-10-05 01:01:10 -0500

    Update owner-lid.js

--- plugins/owner-lid.js
+++ plugins/owner-lid.js
@@ -3,8 +3,7 @@ const userLid = m.sender
 await conn.sendMessage(m.chat, { text: `Tu LID es: ${userLid}` }, { quoted: m })
 }
 
-handler.help = ['mylid', 'lid']
-handler.tags = ['info']
+
 handler.command = ['mylid', 'lid']
 
 export default handler
