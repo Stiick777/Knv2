98ef80f62225bc3ee10a00af57a759e5ee2c0790
Stiick777 <kanixxxcexx@gmail.com>
2025-12-08 11:27:24 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '1'
-import './settings.js'
+import './config.js'
 import { setupMaster, fork } from 'cluster'
 import { watchFile, unwatchFile } from 'fs'
 import cfonts from 'cfonts'
@@ -11,7 +11,7 @@ import fs, {readdirSync, statSync, unlinkSync, existsSync, mkdirSync, readFileSy
 import yargs from 'yargs';
 import {spawn} from 'child_process'
 import lodash from 'lodash'
-import { RubyJadiBot } from './plugins/jadibot-serbot.js';
+import { RubyJadiBot } from './plugins/_yuta.js';
 import chalk from 'chalk'
 setTimeout(async () => {
     try {
