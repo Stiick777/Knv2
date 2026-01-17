99c8d7b8ccc2c71406a0402bf01e6d056360e9f1
Stiick777 <kanixxxcexx@gmail.com>
2026-01-17 18:04:22 -0500

    Update convertidor-toimg.js

--- plugins/convertidor-toimg.js
+++ plugins/convertidor-toimg.js
@@ -1,4 +1,4 @@
-import { webp2png } from '../lib/webp2png.js'
+import { webp2png } from '../lib/webp2mp4.js'
 
 let handler = async (m, { conn, usedPrefix, command }) => {
   const notStickerMessage = `Hubo un Error *${usedPrefix + command}*`
