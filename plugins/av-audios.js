e3cef953ee017e260dfdc0cdea36c2e05dba6b68
Stiick777 <kanixxxcexx@gmail.com>
2026-01-23 11:35:21 -0500

    Update av-audios.js

--- plugins/av-audios.js
+++ plugins/av-audios.js
@@ -1,21 +1,21 @@
-
 let handler = m => m
 handler.all = async function (m) {
   for (const message in audioMsg) {
     if (new RegExp(`^${message}$`, 'i').test(m.text)) {
-      this.sendFile(m.chat, audioMsg[message], 'audio.mp3', null, m, true)
+      await this.sendMessage(m.chat, {
+        audio: { url: audioMsg[message] },
+        mimetype: 'audio/ogg; codecs=opus',
+        ptt: true
+      }, { quoted: m })
       break
     }
   }
   return !0
- }
+}
 
 export default handler
 
-
 let audioMsg = {
   'nadie te preguntó': 'https://qu.ax/cyWlY.mp3',
-  'Nadie te pregunto': 'https://qu.ax/cyWlY.mp3'
-  
+  'nadie te pregunto': 'https://qu.ax/cyWlY.mp3'
 }
-
