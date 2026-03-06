4c221da41be9755f2aa5067640fe2e80244fcc6a
Stiick777 <kanixxxcexx@gmail.com>
2026-03-05 21:34:09 -0500

    Update descargas-play.js

--- plugins/descargas-play.js
+++ plugins/descargas-play.js
@@ -228,9 +228,9 @@ try {
 }
 
 }
-handler.help = [ 'play2'];
+handler.help = [ 'play2', 'play'];
 handler.tags = ['descargas'];
-handler.command = ['play2']
+handler.command = ['play2', 'play']
 handler.group = true;
 export default handler;
 
@@ -326,43 +326,3 @@ if (data.status === 'ok') {
   }
 }
 
-/*
-import yts from 'yt-search';
-import fetch from 'node-fetch';
-let limit = 320;
-let confirmation = {};
-
-let handler = async (m, { conn, command, text, args, usedPrefix }) => {
-    if (!text) throw `✳️ Ejemplo: *${usedPrefix + command}* Lil Peep hate my life`;
-
-    let res = await yts(text);
-    let vid = res.videos[0];
-    if (!vid) throw `✳️ Vídeo/Audio no encontrado`;
-
-    let { title, thumbnail, videoId, timestamp, views, ago, url } = vid;
-
-    m.react('🎧');
-
-    let playMessage = `
-≡ *YOUTUBE MUSIC*
-┌──────────────
-▢ 📌 *Título:* ${title}
-▢ 📆 *Subido hace:* ${ago}
-▢ ⌚ *Duración:* ${timestamp}
-▢ 👀 *Vistas:* ${views.toLocaleString()}
-└──────────────`;
-
-    conn.sendButton(m.chat, playMessage, null, thumbnail, [
-        ['🎶 MP3', `${usedPrefix}yta ${url}`],
-        ['🎥 MP4', `${usedPrefix}ytv ${url}`]
-    ], m);
-};
-
-handler.help = ['play'];
-handler.tags = ['descargas'];
-handler.command = ['play', 'play2'];
-handler.disabled = false;
-handler.group = true;
-
-export default handler;
-*/
