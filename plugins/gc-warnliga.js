0e7ee1d59fd4eae38afdcb706626e703868f5449
Stiick777 <kanixxxcexx@gmail.com>
2026-06-10 13:36:02 -0500

    Update gc-warnliga.js

--- plugins/gc-warnliga.js
+++ plugins/gc-warnliga.js
@@ -1,19 +1,13 @@
-import fs from 'fs'
-
-let handler = async (m) => {
-  try {
-    const path = './node_modules/@whiskeysockets/baileys/package.json'
-    const pkg = JSON.parse(fs.readFileSync(path, 'utf8'))
-
-    m.reply(`Nombre: ${pkg.name}
-Versión: ${pkg.version}
-Descripción: ${pkg.description || 'Sin descripción'}`)
-  } catch (e) {
-    m.reply(e.message)
-  }
+let handler = async (m, { conn }) => {
+  m.reply(
+    'Propiedades:\n\n' +
+    Object.keys(conn)
+      .sort()
+      .join('\n')
+  )
 }
 
-handler.command = ['infobaileys']
+handler.command = ['conninfo']
 handler.owner = true
 
 export default handler
